93fe8965b9f570974a8a1dcb53bba981bc3fede0
Ryan Bajzath <ryan@Ryande-MacBook-Air.local>
2026-07-30 16:29:00 +0800

    chore: configure NativeWind and AsyncStorage

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-slate-100">
+      <Text className="text-2xl font-bold text-blue-600">
+        Task Management App
+      </Text>
+
+      <Text className="mt-2 text-base">
+        NativeWind is working.
+      </Text>
     </View>
   );
 }
