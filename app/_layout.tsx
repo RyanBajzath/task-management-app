76ca6c912834daa67dd8184d63a2fda8612050d5
Ryan Bajzath <ryan@Ryande-MacBook-Air.local>
2026-07-31 15:46:37 +0800

    changed header titles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,27 @@ import { TaskProvider } from "../context/TaskContext";
 export default function RootLayout() {
   return (
     <TaskProvider>
-      <Stack />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ title: "Tasks" }}
+        />
+
+        <Stack.Screen
+          name="tasks/new"
+          options={{ title: "Create Task" }}
+        />
+
+        <Stack.Screen
+          name="tasks/[id]"
+          options={{ title: "Task Details" }}
+        />
+
+        <Stack.Screen
+          name="tasks/[id]/edit"
+          options={{ title: "Edit Task" }}
+        />
+      </Stack>
     </TaskProvider>
   );
 }
